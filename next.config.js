f179ad07b2f8b1f3ced0e7275a4a955e9bb44bdb
Tung Hoang <tunghoangdev@gmail.com>
2023-04-26 10:08:25 +0700

    add config image

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "static.showniq.ai",
+                port: "",
+                pathname: "/public/app/**",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
