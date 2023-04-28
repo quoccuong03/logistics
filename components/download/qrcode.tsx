c4f96aa84fced1ef29a30eecc4e57f7c4e78b31f
cuongho <cuongseven789@gmail.com>
2023-04-28 08:59:40 +0700

    udpate code download

--- components/download/qrcode.tsx
+++ components/download/qrcode.tsx
@@ -1,23 +1,34 @@
 "use client";
 import React from "react";
 import { QRCodeSVG } from "qrcode.react";
+import Logo from "@public/images/logoShowniq.svg";
+import Image from "next/image";
+import Link from "next/link";
+const linkConfig = "https://showniq.vn/share?refType=APP";
 export default function CreateQrCode() {
-    return (
-        <div className="mx-auto mt-5 flex items-center justify-center">
-            <div className="flex flex-col mr-10 border p-2 rounded justify-center  text-center">
-                <QRCodeSVG
-                    value="https://play.google.com/store/apps/details?id=com.showniq"
-                    style={{ margin: "auto" }}
-                />
-                <span className="mt-2 uppercase">CH Play</span>
-            </div>
-            <div className="flex flex-col justify-center border p-2 rounded text-center">
-                <QRCodeSVG
-                    value="https://apps.apple.com/vn/app/showniq-stylist-ai-c%E1%BB%A7a-b%E1%BA%A1n/id6445944694"
-                    style={{ margin: "auto" }}
-                />
-                <span className="mt-2 uppercase">App store</span>
-            </div>
-        </div>
-    );
+	return (
+		<div className="mx-auto mt-20 flex items-center  flex-col justify-center">
+			<div className="relative h-[96px] w-[87px] mb-[10px]">
+				<Image
+					src={Logo}
+					alt="img"
+					loading={"lazy"}
+					fill
+					style={{
+						objectFit: "cover",
+					}}
+				/>
+			</div>
+			<div className="text-[16px] font-bold mb-[60px]">Showniq - AI Stylist Của Bạn</div>
+			<div className="flex flex-col p-2 border mb-[5px] rounded justify-center  text-center">
+				<QRCodeSVG value={linkConfig} style={{ margin: "auto" }} />
+			</div>
+			<div className="text-[14px] font-light mb-[60px]">
+				Scan the QR code then you can install the app
+			</div>
+			<Link className="text-[14px] font-light underline" href={"/"}>
+				Go to website
+			</Link>
+		</div>
+	);
 }
