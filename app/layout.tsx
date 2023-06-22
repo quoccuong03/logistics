9c5cfec0e02f2b8cf704e2e6983f1ecacf2c5d37
cuongho2 <cuong.ho@ocmg.vn>
2023-06-22 10:00:58 +0700

    fix code link

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,59 +6,55 @@ import { headers } from "next/headers";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: {
-        template: "%s | SHOWNIQ.AI",
-        default: "SHOWNIQ - AI Stylist Của Bạn",
-    },
-    description: "Có SHOWNIQ - Thời trang thật dễ dàng",
-    icons: {
-        icon: "/favicon.png",
-    },
-    openGraph: {
-        title: {
-            template: "Phong cách %s | SHOWNIQ.AI",
-            default: "SHOWNIQ - AI Stylist Của Bạn",
-        },
-        description: "Có SHOWNIQ - Thời trang thật dễ dàng",
-        url: "https://showniq.ai/",
-        siteName: "SHOWNIQ",
-        images: [
-            {
-                url: "https://showniq.ai/images/og-image.png",
-                width: 800,
-                height: 600,
-            },
-        ],
-        locale: "vi-VN",
-        type: "website",
-    },
-    verification: {
-        other: {
-            "facebook-domain-verification": "d06zgrvc6d7l93u2quk2bv4zbl6pc4",
-        },
-    },
+	title: {
+		template: "%s | SHOWNIQ.AI",
+		default: "SHOWNIQ - AI Stylist Của Bạn",
+	},
+	description: "Có SHOWNIQ - Thời trang thật dễ dàng",
+	icons: {
+		icon: "/favicon.png",
+	},
+	openGraph: {
+		title: {
+			template: "Phong cách %s | SHOWNIQ.AI",
+			default: "SHOWNIQ - AI Stylist Của Bạn",
+		},
+		description: "Có SHOWNIQ - Thời trang thật dễ dàng",
+		url: "https://showniq.ai/",
+		siteName: "SHOWNIQ",
+		images: [
+			{
+				url: "https://showniq.ai/images/og-image.png",
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: "vi-VN",
+		type: "website",
+	},
+	verification: {
+		other: {
+			"facebook-domain-verification": "d06zgrvc6d7l93u2quk2bv4zbl6pc4",
+		},
+	},
 };
 
-export default async function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    const dataJson = await getInfoDonwload();
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const dataJson = await getInfoDonwload();
 
-    const userAgent = headers().get("user-agent");
-    let linQr = "/download";
-    if (dataJson?.qr_link && userAgent) {
-        if (/iPad|iPhone|iPod/.test(userAgent)) {
-            linQr = dataJson?.qr_link;
-        } else if (/Android/.test(userAgent)) {
-            linQr = dataJson?.qr_link;
-        }
-    }
+	const userAgent = headers().get("user-agent");
+	let linQr = "/download";
+	if (dataJson?.qr_link && userAgent) {
+		if (/iPad|iPhone|iPod/.test(userAgent)) {
+			linQr = dataJson?.appStore;
+		} else if (/Android/.test(userAgent)) {
+			linQr = dataJson?.googlePlay;
+		}
+	}
 
-    return (
-        <html lang="en">
-            <Layout linQr={linQr}>{children}</Layout>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<Layout linQr={linQr}>{children}</Layout>
+		</html>
+	);
 }
