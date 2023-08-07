e6349a1159ad8f3d60c3a209c2fc76da2892b96c
Tung Hoang <tunghoangdev@gmail.com>
2023-08-07 14:25:00 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+function getBasePath() {
+    var basePath = "";
+
+    if (process.env.BASE_PATH) {
+        if (process.env.BASE_PATH.startsWith("/")) {
+            basePath = process.env.BASE_PATH;
+        } else {
+            basePath = "/" + process.env.BASE_PATH;
+        }
+    }
+    return basePath;
+}
+
 const nextConfig = {
+    basePath: getBasePath(),
+    i18n: {
+        locales: ["en", "vi", "kr"],
+        defaultLocale: "en",
+    },
     images: {
         remotePatterns: [
             {
