bbb8c78220c7d8042e7de681087a25576a52c5fd
Tung Hoang <tunghoangdev@gmail.com>
2023-08-11 09:33:31 +0700

    fix bug

--- app/api/lang/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const res = await fetch(
-        `${process.env.BASE_URL}/uploads/app/landingpage/config/master.json`
-    );
-    const langConfig = await res.json();
-    return NextResponse.json(langConfig);
-}
