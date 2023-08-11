1204ae60c864c20a35d271a453dfccd0c640e11d
Tung Hoang <tunghoangdev@gmail.com>
2023-08-11 22:28:55 +0700

    fix bug

--- middleware.ts
+++ middleware.ts
@@ -41,7 +41,9 @@ export function middleware(request: NextRequest) {
         // The new URL is now /en-US/products
         return NextResponse.redirect(
             new URL(
-                `${process.env.BASE_PATH}/${i18n.defaultLocale}`,
+                `${process.env.BASE_PATH}/${i18n.defaultLocale}${
+                    newUrl.startsWith("/") ? "" : "/"
+                }${newUrl}`,
                 request.url
             )
         );
