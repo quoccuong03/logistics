a926a041b32c55a80f50c0821c3e6bfcb546c026
Tung Hoang <tunghoangdev@gmail.com>
2023-08-13 09:39:34 +0700

    fix bug

--- components/home/VideoInfo.tsx
+++ components/home/VideoInfo.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { i18n } from "@/config/i18n-config";
-import { usePathname } from "next/navigation";
+import { useLocale } from "@/hooks/useLocale";
 
 // /app/landingpage/home/home-rec-vi.mp4
 // /app/landingpage/home/home-rec-en.mp4
 // /app/landingpage/home/home-rec-kr.mp4
 export default function VideoInfo() {
-    const pathname = usePathname();
-    const segments = pathname?.split("/");
-    const videoHome = `${
-        process.env.NEXT_PUBLIC_BASE_URL
-    }/uploads/app/landingpage/home/home-rec-${
-        segments?.[1] || i18n.defaultLocale
-    }.mp4`;
+    const locale = useLocale();
+
+    const videoHome = `${process.env.NEXT_PUBLIC_BASE_URL}/uploads/app/landingpage/home/home-rec-${locale}.mp4`;
     return (
         <div>
             <video
